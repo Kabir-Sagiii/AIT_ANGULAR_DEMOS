14043ef4e4fb59858ad315285570d5a38b9ca685
Kabir <sagithedeveloper@gmail.com>
2024-02-16 10:47:28 +0530

    added the code

--- S08_16-02-2024/shadowing.js
+++ S08_16-02-2024/shadowing.js
@@ -1,5 +1,13 @@
-var rating = 4.5;
+// var rating = 4.5;
 
-var rating = 5.5;
+// var rating = 5.5;
 
-console.log(rating);
+// console.log(rating);
+
+// let rating = 4.5
+
+// let rating = 6.9
+
+// const code = 99
+
+// const code = 88
