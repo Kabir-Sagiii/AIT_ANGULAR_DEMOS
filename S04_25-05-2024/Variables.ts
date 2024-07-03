beadc93a03da3df993a36ceac477bac5fe6ca1e6
Kabir <sagithedeveloper@gmail.com>
2024-07-03 18:31:48 +0530

    Added the codE

--- S04_25-05-2024/Variables.ts
+++
@@ -1,8 +0,0 @@
-
-// mycity = "sagar" Invalid
-
-var username = "Riya verma"
-
-let usercity = "Delhi"
-
-const pinNumber = 99990
--- S05_27-05-2024/variables.js
+++
@@ -1,4 +0,0 @@
-var cityName = "Hyderabad";
-var userName = "Rohan";
-var id = 100;
-console.log(cityName, userName, id);
